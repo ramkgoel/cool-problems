8e6686bc7e9e9896104b358f391d8fc119994671
ramkgoel <ramkgoel@mit.edu>
2023-11-25 02:43:53 -0500

    added first cf problem

---
+++ template.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+using ll = long long;
+#define all(x) (x).begin(), (x).end()
+ 
+void solve() {
+    cout << "hello";
+}
+ 
+int main() {
+    cin.tie(0)->sync_with_stdio(false);
+ 
+    int t;
+    cin >> t;
+    
+    while (t--)
+        solve();
+    
+    cout << "hii";
+ 
+    return 0;
+}
